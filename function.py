3c1cd79f21012243757e2c729f490db7a99caa1b
lukaskurnia <lukasjonathan99@gmail.com>
2019-09-26 11:59:44 +0700

    Revert "Remove unused comment"
    This reverts commit 780ee0417ed51b80a61ca6b8001ebaf7c236f96f.

--- function.py
+++ function.py
@@ -9,14 +9,24 @@ ACK = b'\x01'
 FIN = b'\x02'
 FIN_ACK = b'\x03'
 
-MAX_DATA = 32768 
+MAX_DATA = 32768 #dalam byte
 LISTEN_PORT = 6789
 MAX_SENT = 33000
 MAX_PROGRESS = 40
 
+<<<<<<< HEAD
 def createListPacket(filename, id):
     data_byte = openFile(filename)
     identifier = id.to_bytes(1, byteorder='big')
+=======
+
+# packet = '0101010101001010101011010010101011010101010101010101010101010101010101010010101010101010101010'
+
+def createListPacket(filename):
+
+    data_byte = openFile(filename)
+    identifier = randomId() 
+>>>>>>> parent of 780ee04... Remove unused comment
     listPacket = []
     count_seq= 1
 
@@ -65,18 +75,31 @@ def breakPacket(packet):        #packet merupakan bytearray
 # return string biner
 def calculateChecksum(tipe, identifier, sequence, length, data):
     packetWOChecksum = ((int.from_bytes(tipe,byteorder='big') << 4) + int.from_bytes(identifier,byteorder='big')).to_bytes(1,byteorder='big') + sequence + length + data    #paket without checksum
-    
+    # print(packetWOChecksum)
+
     # inisiasi
     piecePacket = packetWOChecksum[0:2]           #Mengambil 16 bits pertama
     packetWOChecksum = packetWOChecksum[2:]    
     calculateCheck = int.from_bytes(piecePacket,byteorder='big')
+    # print(calculateCheck)
+    # print(piecePacket)             
     
     while(len(packetWOChecksum)>2):
         satuanPiecePacket = packetWOChecksum[0:2]
         packetWOChecksum = packetWOChecksum[2:]
         calculateCheck = calculateCheck ^ int.from_bytes(satuanPiecePacket,byteorder='big')
-        
+        # print(satuanPiecePacket)
+
+    # #Bit paket tidak kelipatan 2
+    # jumlahTambahNol =  2-(len(packetWOChecksum))
+    
+    # for x in range(jumlahTambahNol):
+    #     # print(type(packetWOChecksum) + " = tipe paketWOceksam")
+    #     packetWOChecksum = packetWOChecksum + str(0)
+
+    # print(packetWOChecksum)
     calculateCheck = calculateCheck ^ int.from_bytes(packetWOChecksum,byteorder='big')
+    # print("calculate check" + str(calculateCheck))
 
     return calculateCheck.to_bytes(2,byteorder='big')
 
@@ -102,12 +125,48 @@ def openFile(filename):
     data.close()
     return dataByte
 
+<<<<<<< HEAD
+=======
+#Menguban menjadi biner: 0b000011101010....
+def toBiner(dataFile):
+    integer=int.from_bytes(dataFile, byteorder='big')
+    biner=bin(integer) 
+    return biner
+
+#Remove 0b from biner
+def removeTag(biner):
+    return biner[2:]
+
+def removeBpetik(paketACK):
+    return paketACK[2:(len(paketACK)-1)]
+
+#Generate Random ID return bytearray with lenght 1 byte
+def randomId():
+    return (random.randint(0, 15)).to_bytes(1, byteorder='big')
+
+#get Integer from biner
+def getInt(biner):
+    return int(biner,2)
+
+#Menambahkan 0b pada biner
+def addTag(binary):
+    return '0b' + binary
+
+>>>>>>> parent of 780ee04... Remove unused comment
 #Menuliskan file dari type data byte
 def writeFile(byte,filename):
+    # integer = int(binary,2)
+    # byte = integer.to_bytes((integer.bit_length()+7)//8, byteorder='big', signed = False)
+
     f = open(filename, "wb+")
     f.write(byte)
     f.close()
 
+
+# f = openFile(input())
+# data_biner = removeTag(toBiner(f))
+# writeFile(data_biner,"output.pdf")
+   
 # create an array and initialize with element
 def initialize(size, element):
     array = [element] * size
